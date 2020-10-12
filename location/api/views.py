efbf0d4e8225db9326062408ee5b9ca5e970f304
Farhad Hossain <farhadhossain0085@gmail.com>
2020-10-13 02:29:44 +0600

    date range in api

--- location/api/views.py
+++ location/api/views.py
@@ -4,6 +4,8 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from rest_framework.permissions import IsAuthenticated
+import dateparser
+import datetime
 
 
 @api_view(['GET', 'POST'])
@@ -11,7 +13,18 @@ from rest_framework.permissions import IsAuthenticated
 def location_list(request):
 
     current_user = request.user
-    
-    locations = Location.objects.filter(uploaded_by=current_user).order_by('-created_time').all()
+
+    if request.method == "POST":
+        start_date = dateparser.parse(request.data['start']).replace(tzinfo=datetime.timezone.utc)
+        end_date = dateparser.parse(request.data['end']).replace(tzinfo=datetime.timezone.utc)
+        
+        locations = Location.objects.filter(
+            uploaded_by=current_user,
+            created_time__gte=start_date,
+            created_time__lte=end_date).order_by('-created_time').all()
+    else:
+        locations = Location.objects.filter(
+            uploaded_by=current_user).order_by('-created_time').all()
+
     serializer = LocationSerializer(locations, many=True)
     return Response(serializer.data)
