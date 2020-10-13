69a72455eb2041e1799c85ed3c06dfa334395011
Farhad Hossain <farhadhossain0085@gmail.com>
2020-10-14 02:26:48 +0600

    location create api done

--- location/api/urls.py
+++ location/api/urls.py
@@ -5,6 +5,7 @@ from rest_framework.authtoken import views as auth_views
 
 urlpatterns = [
     path('locations/', location_list, name="location_list"),
+    path('locations/create/', location_create, name="location_create"),
     
     # auth
     path('auth/', auth_views.obtain_auth_token, name="obtain_auth_token"),
--- location/api/views.py
+++ location/api/views.py
@@ -13,20 +13,49 @@ import datetime
 def location_list(request):
 
     current_user = request.user
+    today = datetime.datetime.utcnow()
+    # print(today)
 
     if request.method == "POST":
         print("Request Data", request.data)
-        date_range = request.data['date_range'].split('-')
-        start_date = dateparser.parse(date_range[0]).replace(tzinfo=datetime.timezone.utc)
-        end_date = dateparser.parse(date_range[1]).replace(tzinfo=datetime.timezone.utc)
-        
-        locations = Location.objects.filter(
-            uploaded_by=current_user.device,
-            created_time__gte=start_date,
-            created_time__lte=end_date).order_by('-created_time').all()
+
+        try:
+            date_range = request.data['date_range'].split('-')
+        except:
+            date_range = ""
+            
+        if len(date_range) == 2:
+            start_date = dateparser.parse(date_range[0]).replace(tzinfo=datetime.timezone.utc)
+            end_date = dateparser.parse(date_range[1]).replace(tzinfo=datetime.timezone.utc)
+            locations = Location.objects.filter(
+                uploaded_by=current_user.device,
+                created_time__gte=start_date,
+                created_time__lte=end_date).order_by('-created_time').all()
+        else:
+            locations = Location.objects.filter(uploaded_by=current_user.device, created_time__date=today).order_by('-created_time').all()
+    
     else:
-        locations = Location.objects.filter(
-            uploaded_by=current_user.device).order_by('-created_time').all()
+        locations = Location.objects.filter(uploaded_by=current_user.device, created_time__date=today).order_by('-created_time').all()
+
     print(locations.count(), "locations found")
     serializer = LocationSerializer(locations, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def location_create(request):
+
+    current_user = request.user
+    
+    request_data = request.data
+
+    location = Location.objects.create(
+        latitude=request_data['latitude'],
+        longitude=request_data['longitude'],
+        uploaded_by=current_user.device
+    )
+
+    print("locations saved")
+    serializer = LocationSerializer(location)
     return Response(serializer.data)
