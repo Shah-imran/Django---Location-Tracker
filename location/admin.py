5a4cc3ce0f1772a01afaf6e1deec0792ae8acc55
Farhad Hossain <farhadhossain0085@gmail.com>
2020-10-14 13:13:52 +0600

    model admin done

--- location/admin.py
+++ location/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Location)
-admin.site.register(Device)
-admin.site.register(Account)
+class LocationAdmin(admin.ModelAdmin):
+    search_fields = ['latitude', 'longitude', 'uploaded_by__user__username']
+    list_display = ['latitude', 'longitude', 'created_time', 'uploaded_by']
+
+class DeviceAdmin(admin.ModelAdmin):
+    search_fields = ['device_id', 'user__username']
+    list_display = ['device_id', 'user', 'is_active']
+
+class AccountAdmin(admin.ModelAdmin):
+    search_fields = ['username', 'email']
+    list_display = ['username', 'email']
+
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Device, DeviceAdmin)
+admin.site.register(Account, AccountAdmin)
