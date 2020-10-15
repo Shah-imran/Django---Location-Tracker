1b24991abdd3aaf89e149d53ab0a4b784f1ca387
Farhad Hossain <farhadhossain0085@gmail.com>
2020-10-15 17:30:47 +0600

    requirements.txt

--- location_tracker/settings/__init__.py
+++ location_tracker/settings/__init__.py
@@ -95,7 +95,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = '/static/'
 
 SITENAME = "Location Tracker"
 TAGLINE = "Track your places seemlessly"
